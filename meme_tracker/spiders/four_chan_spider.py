92f4b75b53bc5dd73e9f25e013482546e7ae7c47
Benjamin Deuson <benjamindeuson@gmail.com>
2019-12-16 22:04:59 -0500

    Add a spider for 4chan threads

---
+++ meme_tracker/spiders/four_chan_spider.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from pathlib import Path
+import scrapy
+from yaml import safe_load
+
+
+class FourChanSpider(scrapy.Spider):
+    name = '4chan_images'
+    start_urls = safe_load(Path('spider_config.yaml').read_text())['urls']
+
+    def parse(self, response):
+        thread_id = response.url.split("/")[-1]
+        download_time = f'{datetime.now().isoformat(timespec="seconds")}'
+        filename = f'{download_time}-{thread_id}.html'
+        with open(filename, 'wb') as outfile:
+            outfile.write(response.body)
+        self.log(f'Saved content of {response.url} to {filename}.')
