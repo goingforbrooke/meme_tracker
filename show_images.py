c5ec0c51a1ca562f54c196305d44d9a28072c822
Benjamin Deuson <benjamindeuson@gmail.com>
2019-12-22 12:09:59 -0500

    Add show_images.py
    This file will run a Flask server that lists all locally
    downloaded images in a grid.

---
+++ show_images.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from pathlib import Path
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_local_images():
+    images = [item for item in Path('saved_images') if item.is_file()]
+    return 'test'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
