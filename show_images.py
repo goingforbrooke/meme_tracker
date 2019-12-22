ba962ebde4071b5568c8d7e6058929307612a74b
Benjamin Deuson <benjamindeuson@gmail.com>
2019-12-22 15:48:08 -0500

    Display images in a grid if they're in static/

--- show_images.py
+++ show_images.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 from pathlib import Path
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def show_local_images():
-    images = [item for item in Path('saved_images') if item.is_file()]
-    return 'test'
+    image_urls = [url_for('static', filename=item.name)
+                  for item in Path('saved_images', 'full').iterdir() if item.is_file()]
+    return render_template('image_grid.html', image_urls=image_urls)
 
 
 if __name__ == '__main__':
